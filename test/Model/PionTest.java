27300ca5307f22b8209c90cfc84bce1dba9f49e6
Indigoblin <axolot.jvc@gmail.com>
2020-03-31 16:27:36 +0200

    second commit

--- test/Model/PionTest.java
+++ test/Model/PionTest.java
@@ -1,4 +1,10 @@
 package Model;
 
 public class PionTest {
+
+    // pion - envoit info sur son état (pas d'action)
+    // pion déjà présent ou pas sur plateau
+    //
+
+
 }
--- test/Model/PlateauTest.java
+++ test/Model/PlateauTest.java
@@ -1,4 +1,19 @@
 package Model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PlateauTest {
+
+    @Test
+    public void plateauEstVide(){
+        Plateau plateau = new Plateau();
+
+        // class field
+        // plateau - envoit info sur son état, ne fait pas d'action
+        // vérifie si victoire horizontal, vertical, diagonale
+        // vérifie si plateau est plein, si oui, partie fini
+        //
+
+    }
 }
