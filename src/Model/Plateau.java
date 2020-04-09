86af662b19621cb0e682353c2f502e5bf3e840e6
Indigoblin <axolot.jvc@gmail.com>
2020-04-09 08:24:01 +0200

    maj board

--- src/Model/Plateau.java
+++ src/Model/Plateau.java
@@ -13,7 +13,18 @@ public class Plateau {
         }
     }
 
+
+    // Personne sur le net semble expliquer comment on remplit un int avec deux arrays list, une seule oui mais deux, aucune mention
+
+    //public int populateCase(int c1, int c2, int c3, int c4, int c5, int c6, int c7, int c8, int c9){
+    //    CaseMorp = new int[][] {4,5,6,7,8};
+    //}
+
     public int returnCase(int i, int i1) {
         return i+i1;
     }
+
+    //public boolean checkPositionVert(boolean vertical){
+    //
+    //}
 }
